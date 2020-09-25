2535526b263a465bc979d941d9b447a5dfedc5da
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-09-25 13:56:34 -0700

    inquirer

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-// Allow app.js file to access the fs module's functions through the fs assignment
-const fs = require('fs');
+// Use prompts from Inquirer
+const inquirer = require('inquirer');
 
-//Import generatePage function from page-template.js
-const generatePage = require('./src/page-template.js');
+// // Allow app.js file to access the fs module's functions through the fs assignment
+// const fs = require('fs');
 
-// Array that holds the user command-line arguments (9.1)
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// //Import generatePage function from page-template.js
+// const generatePage = require('./src/page-template.js');
 
-// Store user command-line arguments as distinct variables -- destructured assignment code (9.2)
-const [name, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            tyoe: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-// Create HTML File (9.2)
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-    console.log('Porfolio complete! Check out index.html to see the output');
-});
+// // Create HTML File (9.2)
+// fs.writeFile('./index.html', pateHTML, err => {
+//     if (err) throw err;
 
-
-
-
+//     console.log('Porfolio complete! Check out index.html to see the output');
+// });
 
 
 
 
+
+
+
+//Lesson 9.2 Original Code
+// // Array that holds the user command-line arguments (9.1)
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// // Store user command-line arguments as distinct variables -- destructured assignment code (9.2)
+// const [name, github] = profileDataArgs;
 
 
 // Lesson 9.1 Original Code
